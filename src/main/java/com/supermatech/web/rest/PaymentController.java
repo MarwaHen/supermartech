83ba7e5bd01fe9fa32d817a8557f3ca67d65232c
leonardv <valentin.leonard@outlook.fr>
2024-09-12 14:25:58 +0200

    Update payment controller

--- src/main/java/com/supermatech/web/rest/PaymentController.java
+++ src/main/java/com/supermatech/web/rest/PaymentController.java
@@ -1,8 +1,5 @@
 package com.supermatech.web.rest;
 
-import com.nimbusds.jose.shaded.gson.JsonArray;
-import com.nimbusds.jose.shaded.gson.JsonElement;
-import com.nimbusds.jose.shaded.gson.JsonObject;
 import com.supermatech.domain.Order;
 import com.supermatech.domain.Product;
 import io.swagger.v3.core.util.Json;
@@ -12,7 +9,6 @@ import jakarta.persistence.PersistenceContext;
 import java.util.*;
 import org.hibernate.PessimisticLockException;
 import org.hibernate.exception.LockTimeoutException;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 class PaymentProduct {
@@ -51,12 +47,10 @@ public class PaymentController {
         int id_client = (int) post.get("id_client");
         String adresse = (String) post.get("address");
         String phone_number = (String) post.get("phone_number");
-        JsonArray jsonArray = (JsonArray) post.get("cart_list");
+        String jsonArray = (String) post.get("cart_list");
+        res.put("return", jsonArray);
+
         ArrayList<PaymentProduct> paymentProducts = new ArrayList<>();
-        for (JsonElement jsonElement : jsonArray) {
-            JsonObject jsonObject = jsonElement.getAsJsonObject();
-            paymentProducts.add(new PaymentProduct(jsonObject.get("product_id").getAsInt(), jsonObject.get("quantity").getAsInt()));
-        }
         ArrayList<Quantity> products = new ArrayList<Quantity>();
         boolean isValid = true;
         ArrayList<PaymentProduct> quantityError = new ArrayList<>();
